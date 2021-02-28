bf26250c5a6a5026b206d26e3712493cdbd97899
hortencia718 <hortenciaecisneros@gmail.com>
2021-02-28 16:49:46 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function getMessage(){
+
+
+let message = document.getElementById('entry').value;
+document.getElementById('message').innerHTML = message; 
+
+document.getElementById('entry').value = " ";
+
+}
